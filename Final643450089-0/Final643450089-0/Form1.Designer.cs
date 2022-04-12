4a03c861a1638459a37f4f8db003ae6cf425905c
suthada <96216472+suthada@users.noreply.github.com>
2022-04-12 20:22:44 +0700

    Final643450089-0

--- Final643450089-0/Final643450089-0/Form1.Designer.cs
+++ Final643450089-0/Final643450089-0/Form1.Designer.cs
@@ -116,14 +116,16 @@
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
             // pictureBoxA02s
             // 
--- Final643450089-0/Final643450089-0/Form1.cs
+++ Final643450089-0/Final643450089-0/Form1.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace Final643450089_0
 {
     public partial class Form1 : Form
@@ -7,6 +9,7 @@ namespace Final643450089_0
         SaleManagement saleManagement;
         Product product;
         Bill bill;
+        Coupon coupon;
 
         private double sum1;
         private double sum2;
@@ -31,27 +34,63 @@ namespace Final643450089_0
             dataGridView1.Rows[n].Cells[3].Value = Change;
             dataGridView1.Rows[n].Cells[4].Value = Coupon;
 
-            /*int price = 0, total = 0, getChange = 0;
-            if (textBoxPrice.Text == "")
-            {
-                textBoxPrice.SelectAll();
-                return;
-            }
-            if(textBoxTotalPrice.Text == "")
-            {
-                textBoxTotalPrice.SelectAll();
-                return ;
-            }
-            price = Convert.ToInt32(textBoxPrice.Text);
-            TotalPrice = Convert.ToInt32(textBoxTotalPrice.Text);
-            Change = Price - TotalPrice;
-            textBoxTotalPrice.Text = Change;*/
+        }
 
-        }
-        private void displayProduct()
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            //this.pictureBoxA02s.Image = 
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSV (*.csv) | *.csv";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string[] readAllLine = File.ReadAllLines(openFileDialog.FileName);
+
+                string readAllText = File.ReadAllText(openFileDialog.FileName);
+                for (int i = 0; i < readAllLine.Length; i++)
+                {
+                    string allDATARAW = readAllLine[i];
+                    string[] allDATASplited = allDATARAW.Split(',');
+                    this.dataGridView2.Rows.Add(allDATASplited[0], allDATASplited[1], allDATASplited[2], allDATASplited[3]);
+                }
+            }
         }
-        
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView2.Rows.Count > 0)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "CSV(*.csv)|*.csv";
+                bool fileError = false;
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    if (!fileError)
+                    {
+                        try
+                        {
+                            int columnCount = dataGridView2.Columns.Count;
+                            string column = "";
+                            string[] outputCSV = new string[dataGridView2.Rows.Count + 1];
+                            for (int i = 0; i < columnCount; i++)
+                            {
+                                column += dataGridView2.Columns[i].HeaderText.ToString() + ",";
+                            }
+                            outputCSV[0] += column;
+                            for (int i = 1; (i - 1) < dataGridView2.Rows.Count; i++)
+                            {
+                                for (int j = 0; j < columnCount; j++)
+                                {
+                                    outputCSV[i] += dataGridView2.Rows[i - 1].Cells[j].Value.ToString() + ",";
+                                }
+                            }
+                            File.WriteAllLines(saveFileDialog.FileName, outputCSV, Encoding.UTF8);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error :" + ex.Message);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
